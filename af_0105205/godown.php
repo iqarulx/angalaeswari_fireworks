0af1a59ec346a4c6d445d0d5ccd7e325f465a9fd
Arul <arulmurugan2024@srisoftwarez.in>
2025-05-02 15:36:40 +0530

    staff permission

--- af_0105205/godown.php
+++ af_0105205/godown.php
@@ -1,5 +1,5 @@
 <?php 
-	$page_title = "Expense Category";
+	$page_title = "Godown";
 	include("include_user_check.php");
 	$page_number = $GLOBALS['page_number']; $page_limit = $GLOBALS['page_limit'];
 
@@ -7,9 +7,16 @@
     if(isset($_SESSION[$GLOBALS['site_name_user_prefix'].'_user_id']) && !empty($_SESSION[$GLOBALS['site_name_user_prefix'].'_user_id'])) {
         if(!empty($GLOBALS['user_type']) && $GLOBALS['user_type'] != $GLOBALS['admin_user_type']) {
             $login_staff_id = $_SESSION[$GLOBALS['site_name_user_prefix'].'_user_id'];
-            $permission_module = $GLOBALS['expense_category_module'];
+            $permission_module = $GLOBALS['godown_module'];
             include("permission_check.php");
         }
+    }
+
+    $godown_count = 0;
+    $godown_list = array();
+    $godown_list = $obj->getTableRecords($GLOBALS['godown_table'], '', '', '');
+    if(!empty($godown_list)) {
+        $godown_count = count($godown_list);
     }
 ?>
 <!DOCTYPE html>
@@ -18,8 +25,7 @@
 	<title> <?php if(!empty($project_title)) { echo $project_title; } ?> - <?php if(!empty($page_title)) { echo $page_title; } ?> </title>
 	<?php 
 	include "link_style_script.php"; ?>
-    <script type="text/javascript" src="include/js/payment.js"></script>
-
+    <script type="text/javascript" src="include/js/creation_modules.js"></script>
 </head>	
 <body>
 <?php include "header.php"; ?>
@@ -32,34 +38,39 @@
                         <div class="card">
                             <div class="border card-box d-none add_update_form_content" id="add_update_form_content" ></div>
                             <div class="border card-box" id="table_records_cover">
-                                <div class="card-header align-items-center">
-                                    <form name="table_listing_form" method="post">
+                                <form name="table_listing_form" method="post">
+
+                                    <div class="card-header align-items-center">
                                         <div class="row justify-content-end p-2">
                                             <div class="col-lg-3 col-md-4 col-6">
                                                 <div class="input-group">
-                                                    <input type="text" name="search_text" class="form-control" style="height:34px;" placeholder="Search By Name" aria-label="Search" aria-describedby="basic-addon2" onkeyup="Javascript:table_listing_records_filter();">
-                                                    <span class="input-group-text" style="height:34px;" id="basic-addon2"><i class="bi bi-search"></i></span>
+                                                    <input type="text"  name="search_text" onkeyup="Javascript:table_listing_records_filter();" class="form-control" style="height:34px;" placeholder="Search By Name / Location" aria-label="Search" aria-describedby="basic-addon2">
+                                                    <span class="input-group-text" onchange ="Javascript:table_listing_records_filter();"  style="height:34px;" id="basic-addon2"><i class="bi bi-search"></i></span>
                                                 </div>
-                                            </div> <?php
-                                            $access_error = "";
-                                            if(!empty($login_staff_id)) {
-                                                $permission_action = $add_action;
-                                                include('permission_action.php');
-                                            }
-                                            if(empty($access_error)) { ?>
-                                                <div class="col-lg-2 col-md-2 col-3">
+                                            </div>
+                                            <div class="col-lg-2 col-md-2 col-3">
+                                                <?php
+                                                $add_access_error = "";
+                                                if(!empty($login_staff_id)) {
+                                                    $permission_action = $add_action;
+                                                    include('permission_action.php');
+                                                }
+                                                if(empty($add_access_error)) {
+                                                    if($godown_count < $GLOBALS['max_godown_count']) { 
+                                                        ?>
                                                     <button class="btn btn-danger float-end" style="font-size:11px;" type="button" onclick="Javascript:ShowModalContent('<?php if(!empty($page_title)) { echo $page_title; } ?>', '');"> <i class="fa fa-plus-circle"></i> Add </button>
-                                                </div> <?php 
-                                            } ?>
+                                                    <?php }
+                                                } ?>
+                                            </div>
                                             <div class="col-sm-6 col-xl-8">
                                                 <input type="hidden" name="page_number" value="<?php if(!empty($page_number)) { echo $page_number; } ?>">
                                                 <input type="hidden" name="page_limit" value="<?php if(!empty($page_limit)) { echo $page_limit; } ?>">
                                                 <input type="hidden" name="page_title" value="<?php if(!empty($page_title)) { echo $page_title; } ?>">
                                             </div>	
                                         </div>
-                                    </form>
-                                </div>
-                                <div id="table_listing_records"></div>
+                                    </div>
+                                    <div id="table_listing_records"></div>
+                                </form>
                             </div>
                         </div>   
                     </div>
@@ -70,7 +81,7 @@
 <?php include "footer.php"; ?>
 <script>
     $(document).ready(function(){
-        $("#expensecategory").addClass("active");
+        $("#godowncreation").addClass("active");
         table_listing_records_filter();
     });
 </script>
